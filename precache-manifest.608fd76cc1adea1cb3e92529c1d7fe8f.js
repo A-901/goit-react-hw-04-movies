56a66692393d500b70072d411d6c08fdc73027d6
Alexandr <e7000@ukr.net>
2020-06-21 20:06:34 +0300

    Updates

---
+++ precache-manifest.608fd76cc1adea1cb3e92529c1d7fe8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c254b442e16b93eee96d7be91cdf7d89",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "7a78155cd0e9cb252eb1",
+    "url": "/goit-react-hw-04-movies/static/css/main.0ee9615c.chunk.css"
+  },
+  {
+    "revision": "88fde1f2603a190bc9f8",
+    "url": "/goit-react-hw-04-movies/static/js/0.c13e63ec.chunk.js"
+  },
+  {
+    "revision": "6ce1df61c3857d27d3bb",
+    "url": "/goit-react-hw-04-movies/static/js/8.71cea086.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/8.71cea086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c27fe35a6460413edb",
+    "url": "/goit-react-hw-04-movies/static/js/Cast-page.182ee489.chunk.js"
+  },
+  {
+    "revision": "576826c8f7bef5122ef0",
+    "url": "/goit-react-hw-04-movies/static/js/HomePage-page.237b298c.chunk.js"
+  },
+  {
+    "revision": "06467ee1b0223768f1ad",
+    "url": "/goit-react-hw-04-movies/static/js/MovieDetailsPage-page.5adb8c38.chunk.js"
+  },
+  {
+    "revision": "0034850f12b81ed5901b",
+    "url": "/goit-react-hw-04-movies/static/js/MoviesPage-page.fce7562a.chunk.js"
+  },
+  {
+    "revision": "3a6875ef22d6d8d5813b",
+    "url": "/goit-react-hw-04-movies/static/js/Reviews-page.49de87e5.chunk.js"
+  },
+  {
+    "revision": "7a78155cd0e9cb252eb1",
+    "url": "/goit-react-hw-04-movies/static/js/main.5dc7ddd9.chunk.js"
+  },
+  {
+    "revision": "6dcfa9d771bb3db8de67",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.6b490f5b.js"
+  }
+]);
